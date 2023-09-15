2d7378a60b9f1edc5219a953d4cfa8018ce3ff30
yurifws <yurifws@gmail.com>
2023-09-15 01:39:41 -0300

    HIS-2023000 mapeamento de entidades, dtos e domains

--- src/main/java/br/com/pitang/appcarusers/adapters/web/in/users/dto/UserDto.java
+++ src/main/java/br/com/pitang/appcarusers/adapters/web/in/users/dto/UserDto.java
@@ -1,5 +1,31 @@
 package br.com.pitang.appcarusers.adapters.web.in.users.dto;
 
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import br.com.pitang.appcarusers.application.domain.cars.Car;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDto {
 
+	private String firstName;
+	private String lastName;
+	private String email;
+	private Date birthday;
+	private String login;
+	private String password;
+	private String phone;
+	private List<Car> cars;
 }
--- src/main/java/br/com/pitang/appcarusers/application/domain/cars/Car.java
+++ src/main/java/br/com/pitang/appcarusers/application/domain/cars/Car.java
@@ -1,5 +1,24 @@
 package br.com.pitang.appcarusers.application.domain.cars;
 
+import br.com.pitang.appcarusers.application.domain.users.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Car {
+	
+	private Long id;
+	private Integer year;
+	private String licensePlate;
+	private String model;
+	private String color;
+	private User user;
 
 }
