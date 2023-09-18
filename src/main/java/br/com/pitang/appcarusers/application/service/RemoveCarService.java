90555dd4f9a344558cbb2c71b4ccbecc0964b56a
yurifws <yurifws@gmail.com>
2023-09-18 11:33:54 -0300

    HIS-2023200 Implementacao classes application de car

--- src/main/java/br/com/pitang/appcarusers/application/service/RemoveCarService.java
+++ src/main/java/br/com/pitang/appcarusers/application/service/RemoveCarService.java
@@ -1,7 +1,25 @@
 package br.com.pitang.appcarusers.application.service;
 
-import br.com.pitang.appcarusers.application.ports.in.RemoveCarUseCase;
+import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
 
+import org.springframework.stereotype.Component;
+
+import br.com.pitang.appcarusers.application.ports.in.RemoveCarUseCase;
+import br.com.pitang.appcarusers.application.ports.in.SearchCarUseCase;
+import br.com.pitang.appcarusers.application.ports.out.RemoveCarPort;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
 public class RemoveCarService implements RemoveCarUseCase {
 
+	private final RemoveCarPort port;
+	private final SearchCarUseCase searchCarUseCase;
+	
+	@Override
+	public void removeById(Long id) {
+		if(isNotEmpty(searchCarUseCase.searchById(id))) {
+			port.removeById(id);
+		}
+	}
 }
--- src/main/java/br/com/pitang/appcarusers/application/service/SearchCarService.java
+++ src/main/java/br/com/pitang/appcarusers/application/service/SearchCarService.java
@@ -1,7 +1,32 @@
 package br.com.pitang.appcarusers.application.service;
 
-import br.com.pitang.appcarusers.application.ports.in.SearchCarUseCase;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.stereotype.Component;
+
+import br.com.pitang.appcarusers.application.domain.cars.Car;
+import br.com.pitang.appcarusers.application.ports.in.SearchCarUseCase;
+import br.com.pitang.appcarusers.application.ports.out.SearchCarPort;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
 public class SearchCarService implements SearchCarUseCase {
 
+	private final SearchCarPort port;
+	
+	@Override
+	public List<Car> searchAll() {
+		return port.searchAll();
+	}
+
+	@Override
+	public Car searchById(Long id) {
+		Optional<Car> optionalCar = port.searchById(id);
+		if(optionalCar.isPresent()) {
+			return optionalCar.get();
+		}
+		return null;
+	}
 }
